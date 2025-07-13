306009eab15fc65d84d06852eebc147c6394bb72
Jelvys Triana <jelvysc@gmail.com>
2025-07-13 07:31:10 -0400

    Add example de variable environment

--- rxconfig.py
+++ rxconfig.py
@@ -1,15 +1,23 @@
 import reflex as rx
+import os
+from dotenv import load_dotenv
 
-config = rx.Config(
-    app_name="pacta",
-    backend_packages=["bcrypt", "sqlalchemy", "python-jose[cryptography]", "passlib"],
-    backend_port=8000,
-    frontend_port=3000,
-    api_url="http://localhost:8000",
-    db_url="sqlite:///./pacta.db",
-    env_path=".env",
-    log_level="INFO",
-    log_style="pretty",
-    log_file=None,
-    telemetry_enabled=False,
-)
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()
+
+try:
+    config = rx.Config(
+        app_name="pacta",
+        backend_packages=["bcrypt", "sqlalchemy", "python-jose[cryptography]", "passlib"],
+        backend_port=os.getenv("BACKEND_PORT"),
+        frontend_port=os.getenv("FRONTEND_PORT"),
+        api_url=f"{os.getenv('API_URL')}:{os.getenv('FRONTEND_PORT')}",
+        db_url=os.getenv("DB_URL"),
+        env_path=".env",
+        log_level="INFO",
+        log_style="pretty",
+        log_file=None,
+        telemetry_enabled=False,
+    )
+except Exception as e:
+    print(f"Error al configurar Reflex: {e}")
